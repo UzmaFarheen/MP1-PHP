6090677b89c10d5184cf021c20da4e0fd7945536
UzmaFarheen <unln@hawk.iit.edu>
2015-11-08 16:27:59 -0600

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -10,35 +10,27 @@
 <div class="fotorama" data-width="700" data-ratio="600/465" data-max-width="100%">
 <?php
 session_start();
-$email = $_POST["email"];
-echo $email;
 require 'vendor/autoload.php';
-$rds = new Aws\Rds\RdsClient([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
+# Creating a client for the s3 bucket
+use Aws\Rds\RdsClient;
+$client = new Aws\Rds\RdsClient([
+ 'version' => 'latest',
+ 'region'  => 'us-east-1'
 ]);
-$result = $rds->describeDBInstances(array(
-    'DBInstanceIdentifier' => 'ITMO544-MP1-DB'
-   
-));
+$result = $client->describeDBInstances([
+    'DBInstanceIdentifier' => 'MP1-DB',
+]);
+$endpoint = "";
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
-    echo "============\n". $endpoint . "================";
-$link = mysqli_connect($endpoint,"UzmaFarheen","UzmaFarheen","MiniProject");
+# Connecting to the database
+$link = mysqli_connect($endpoint,"UzmaFarheen","UzmaFarheen") or die("Error " . mysqli_error($link));
+/* Checking the database connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-else {
-echo "Success";
-}
-$link->real_query("SELECT * FROM MP1 WHERE email = '$email'");
+$link->real_query("SELECT * FROM MP1");
 $res = $link->use_result();
-echo "Result set order...\n";
-while ($row = $res->fetch_assoc()) {
-    echo "<img src =\" " . $row['raws3url'] . "\" /><img src =\"" .$row['finisheds3url'] . "\"/>";
-echo $row['id'] . "Email: " . $row['email'];
-}
-$link->close();
 ?>
 
 </div>
