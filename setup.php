b556024eaa791c97eb64f503a774ef25e4708db1
UzmaFarheen <unln@hawk.iit.edu>
2015-11-08 22:59:05 -0600

    Update setup.php
    setup works perfectly now

--- setup.php
+++ setup.php
@@ -3,13 +3,23 @@ $rds = new Aws\Rds\RdsClient([
  'version' => 'latest',
  'region'  => 'us-east-1'
 ]);
-$result = $rds->describeDBInstances(array(
- 'DBInstanceIdentifier' => 'mp1'
 
-));
+$s3 = new Aws\S3\S3Client([
+ 'version' => 'latest',
+ 'region'  => 'us-east-1'
+]);
+
+$result = $rds->describeDBInstances([
+ 'DBInstanceIdentifier' => 'mp1'
+]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
- echo "============\n". $endpoint . "================";
-$link = new mysqli($endpoint,"UzmaFarheen","UzmaFarheen") or die("Error " . mysqli_error($link)); 
+ echo "============\n". $endpoint . "================\n";
+ $link = new mysqli($endpoint,"UzmaFarheen","UzmaFarheen","TestProject") or die("Error " . mysqli_error($link)); 
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+ \n";
+ exit();
+}
 $link->query("CREATE TABLE ITMO544 
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -18,7 +28,7 @@ email VARCHAR(20),
 phoneforsms VARCHAR(20),
 raws3url VARCHAR(256),
 finisheds3url VARCHAR(256),
-jpegfilename VARCHAR(256),
+filename VARCHAR(256),
 state tinyint(3) CHECK(state IN(0,1,2)),
 datetime timestamp
 )");
