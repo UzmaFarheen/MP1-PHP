4b1fc6a3f2b76b38797cbcb7b0bf657fab4dbbbc
UzmaFarheen <unln@hawk.iit.edu>
2015-11-09 07:21:05 -0600

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,4 +1,13 @@
-
+<html>
+<head><title>Gallery</title>
+  <!-- jQuery -->
+  <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <!-- Fotorama -->
+  <link href="fotorama.css" rel="stylesheet">
+  <script src="fotorama.js"></script>
+</head>
+<body>
+<div class="fotorama" data-width="700" data-ratio="700/467" data-max-width="100%">
 <?php
 session_start();
 require 'vendor/autoload.php';
@@ -8,13 +17,13 @@ $client = new Aws\Rds\RdsClient([
  'version' => 'latest',
  'region'  => 'us-east-1'
 ]);
-$result = $client->describeDBInstances([
+$result = $client->describeDBInstances(array(
     'DBInstanceIdentifier' => 'mp1',
-]);
-$endpoint = "";
+));
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+echo "============\n". $endpoint . "================";
 # Connecting to the database
-$link = mysqli_connect($endpoint,"UzmaFarheen","UzmaFarheen") or die("Error " . mysqli_error($link));
+$link = mysqli_connect($endpoint,"UzmaFarheen","UzmaFarheen","Project") or die("Error " . mysqli_error($link));
 /* Checking the database connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
@@ -23,17 +32,4 @@ if (mysqli_connect_errno()) {
 $link->real_query("SELECT * FROM ITMO544");
 $res = $link->use_result();
 ?>
-<!DOCTYPE html>
-<html>
-<head><title>Gallery</title>
-  <!-- jQuery -->
-  <!-- Fotorama -->
-  <link href='fotorama.css' rel='stylesheet' type='text/css' >
-  <script src="fotorama.js"></script>
-</head>
-<body>
-  <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-<div class="fotorama" data-width="700" data-ratio="600/465" data-max-width="100%">
-</div>
-</body>
-</html>
+
